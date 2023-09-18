074ec09a286e86324e834050cdef49263e0a567b
adorableco <tpdusdl1001@knu.ac.kr>
2023-09-18 23:53:08 +0900

    Update Auth.js UI

--- cake/src/components/Auth.js
+++ cake/src/components/Auth.js
@@ -1,24 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCakeCandles } from "@fortawesome/fontawesome-free";
 import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import "./style.css";
 
 const Auth = () => {
   return (
     <div className="authContainer">
-      <FontAwesomeIcon
+      <h1>CAKE</h1>
+      {/* <FontAwesomeIcon
         icon={faCakeCandles}
         color={"#04AAFF"}
-        size="3x"
+        size="5x"
         style={{ marginBottom: 30 }}
-      />
+      /> */}
 
       <div className="authBtns">
-        <button name="google">
-          Continue with Google <FontAwesomeIcon icon={faGoogle} />
+        <button name="google" className="authBtn">
+          Continue with Google <FontAwesomeIcon icon={faGoogle} size="2x" />
         </button>
-        <button name="github">
-          Continue with Github <FontAwesomeIcon icon={faGithub} />
+        <button name="github" className="authBtn">
+          Continue with Github <FontAwesomeIcon icon={faGithub} size="2x" />
         </button>
       </div>
     </div>
